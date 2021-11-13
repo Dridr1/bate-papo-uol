eb19aec995ce4f1466c06f9c2c56b4baac1192a5
Dridr1 <a.driano@outlook.com>
2021-11-13 18:17:21 -0300

    As mensagens agora aparecem na tela e a cada mensagem nova a janela dá um sroll até o final mostrando a última mensagem. Todos os 3 tipos de mensagens estão estilizados e mensagens privadas só aparecerão se a mensagem for destinada ao usuário

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Tudo relacionado ao login do usuário e manter sua conexão com o servidor
 let username;
 
 function login(){
@@ -20,6 +21,7 @@ function loginSuccess(){
     document.querySelector('.login-screen').classList.add('login-success');
 
     setInterval(keepConnection, 5000);
+    setInterval(getMessages, 3000);
 }
 
 function loginFailed(){
@@ -40,4 +42,47 @@ function connectionSuccess(){
 }
 function connectionFailed(){
     console.log('connection lost!');
+}
+
+//Tudo relacionado as mensagens exibidas na tela
+
+function getMessages(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    console.log(promise);
+    promise.then(displayMessages);
+}
+
+function displayMessages(promise){
+    document.querySelector('main').innerHTML = '';
+    for(let i = 0; i < promise.data.length; i++){
+        let text;
+        
+        if(promise.data[i].type === 'status'){
+            if(i === (promise.data.length-1)){
+                text = `<span class="message message-status last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> ${promise.data[i].text}</span>`;
+            }
+            else{
+                text = `<span class="message message-status"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> ${promise.data[i].text}</span>`;
+            }
+        }
+        else if(promise.data[i].type === 'message'){
+            if(i === (promise.data.length-1)){
+                text = `<span class="message last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+            }
+            else{
+                text = `<span class="message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+            }
+        }
+        else if(promise.data[i].type === 'private_message'){
+            if(i === (promise.data.length-1)){
+                text = `<span class="message private-message last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> reservadamente para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+            }
+            else{
+                text = `<span class="message private-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> reservadamente para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+            }
+        }
+        document.querySelector('main').innerHTML+=text;
+    }
+    let elemento = document.querySelector('.last-message');
+    elemento.scrollIntoView();
 }
