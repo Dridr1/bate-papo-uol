b43c7ee6cbd694954ba223e87d8399e32ea0e0d6
Dridr1 <a.driano@outlook.com>
2021-11-15 23:48:00 -0300

    Correções aplicadas à função de exibir mensagens, agora o chat só recarrega as mensagens e dá o auto scroll quando uma mensagem nova e recebida e pode ser visualizada pelo usuário. Além disso, agora o usuário tem uma legenda abaixo do input para saber para quem está enviando e se está pública ou privada. Com isso, todos os requisitos do projeto foram cumpridos

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let username;
 let toWho = 'Todos';
 let whoCanSee = 'message';
 let firstExecution = false;
+let oldMessages = [{from: 'controle', to: 'controle', text: 'controle', type: 'controle', time: 'controle'}];
 
-//Tudo relacionado ao login do usuário e manter sua conexão com o servidor
 function login(){
     username = document.querySelector('.username-login').value;
 
@@ -19,23 +19,20 @@ function login(){
     promise.then(loginSuccess);
     promise.catch(loginFailed);
 }
-
 function loginSuccess(){
     document.querySelector('.login-screen').classList.add('login-success');
     getMessages();
     getParticipants();
     setInterval(keepConnection, 5000);
-    //setInterval(getMessages, 5000);
+    setInterval(getMessages, 3000);
     setInterval(getParticipants, 10000);
 }
-
 function loginFailed(){
     document.querySelector('.username-login').classList.remove('hidden-element');
     document.querySelector('.enter-button').classList.remove('hidden-element');
     document.querySelector('.loading').classList.remove('lds-roller');
     document.querySelector('.username-unavailable').classList.remove('hidden-element');
 }
-
 function keepConnection(){
     const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: username});
 
@@ -49,77 +46,38 @@ function connectionFailed(){
     console.log('connection lost!');
     window.location.reload();
 }
-
-//Tudo relacionado as mensagens exibidas na tela
 function getMessages(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-    //console.log(promise);
-    promise.then(test);
+    promise.then(displayMessages);
     promise.catch(()=>{console.log('falha ao requisitar mensagens')});
 }
-function test(promise){
-    let newData= filterMessages(promise.data);
-    let messages = document.querySelector('main');
-    messages.innerHTML = '';
-    if(!firstExecution){
-        for (let i = 0; i < newData.length; i++) {
-            if (newData[i].type === 'status'){
-                messages.innerHTML += `<span class="message message-status last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> ${newData[i].text}</span>`;
-            }
-            else if (newData[i].type === 'message'){
-                messages.innerHTML += `<span class="message last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> para <b>${newData[i].to}</b>: ${newData[i].text}</span>`;
-            }
-            else if (newData[i].type === 'private_message') {
-                messages.innerHTML += `<span class="message private-message last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> reservadamente para <b>${newData[i].to}</b>: ${newData[i].text}</span>`;
-            }
-            scrollToBottom();
-        }
-        //firstExecution = true;
-    }
-    console.log(newData);
-
-}
-/*
-let oldMessages = [];
 function displayMessages(promise){
     let messages = document.querySelector('main');
     let newData = filterMessages(promise.data);
-    if(!firstExecution){
+    let isThereNewContent = true;
+    let lastOldMessage = oldMessages[oldMessages.length-1];
+    let lastNewMessage = newData[newData.length-1];
+    if(lastOldMessage.from === lastNewMessage.from && lastOldMessage.type === lastNewMessage.type && lastOldMessage.time === lastNewMessage.time && lastOldMessage.text === lastNewMessage.text){
+        isThereNewContent = false
+    }
+    if(isThereNewContent){
+        messages.innerHTML = '';
         for (let i = 0; i < newData.length; i++) {
             if (newData[i].type === 'status'){
-                messages.innerHTML += `<span class="message message-status last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> ${promise.data[i].text}</span>`;
+                messages.innerHTML += `<span data-identifier="message" class="message message-status last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> ${newData[i].text}</span>`;
             }
             else if (newData[i].type === 'message'){
-                messages.innerHTML += `<span class="message last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+                messages.innerHTML += `<span data-identifier="message" class="message last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> para <b>${newData[i].to}</b>: ${newData[i].text}</span>`;
             }
             else if (newData[i].type === 'private_message') {
-                messages.innerHTML += `<span class="message private-message last-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> reservadamente para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
+                messages.innerHTML += `<span data-identifier="message" class="message private-message last-message"><em>(${newData[i].time})</em> <b>${newData[i].from}</b> reservadamente para <b>${newData[i].to}</b>: ${newData[i].text}</span>`;
             }
-            scrollToBottom();
         }
-        firstExecution = true;
-    }
-    else{
-        newData = compareData(oldMessages ,filterMessages(promise.data));
-
-        if(newData.length > 0){
-            for (let i = 0; i < newData.length; i++) {
-                if (newData[i].type === 'status'){
-                    messages.innerHTML += `<span class="message message-status"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> ${promise.data[i].text}</span>`;
-                }
-                else if (newData[i].type === 'message'){
-                    messages.innerHTML += `<span class="message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
-                }
-                else if (newData[i].type === 'private_message') {
-                    messages.innerHTML += `<span class="message private-message"><em>(${promise.data[i].time})</em> <b>${promise.data[i].from}</b> reservadamente para <b>${promise.data[i].to}</b>: ${promise.data[i].text}</span>`;
-                }
-            }
-            scrollToBottom();
-        }
+        const lastMessage = document.querySelector('main').lastChild;
+        lastMessage.scrollIntoView();
     }
     oldMessages = newData;
 }
-*/
 function filterMessages(data){
     let arrAux = [];
     for (let i = 0; i < data.length; i++) {
@@ -127,26 +85,8 @@ function filterMessages(data){
             arrAux.push(data[i]);
         }
     }
-
     return arrAux;
 }
-function compareData(oldData, newData){
-    let arrAux = [];
-    for (let i = newData.length-1; i > 0; i--) {
-        if(oldData[oldData.length-1] !== newData[i]){
-            arrAux.unshift(newData[i]);
-        }
-        else if(oldData[oldData.length-1] === newData[i]){
-            break;
-        }
-    }
-    return arrAux;
-}
-function scrollToBottom() {
-    const lastMessage = document.querySelector('main').lastChild;
-    lastMessage.scrollIntoView();
- }
-//Funções relacionadas ao envio de mensagens
 function sendMessage(){
     const typedMessage = document.querySelector('.input-message').value;
     document.querySelector('.input-message').value = '';
@@ -162,8 +102,6 @@ function sendMessage(){
         window.location.reload();
     })
 }
-
-//Funções relacionadas ao menu
 function openMenu(){
     document.querySelector('.menu').style.zIndex = "3";
     
@@ -180,8 +118,16 @@ function closeMenu(){
     setTimeout(() => {
         document.querySelector('.menu').style.zIndex = "-1"
     }, 500);
+    if(whoCanSee === 'private_message'){
+        document.querySelector('.message-detail').innerHTML = `Enviando para ${toWho} (reservadamente)`;
+    }
+    else if(whoCanSee === 'message' && toWho !== 'Todos'){
+        document.querySelector('.message-detail').innerHTML = `Enviando para ${toWho} (público)`;
+    }
+    else{
+        document.querySelector('.message-detail').innerHTML = '';
+    }
 }
-
 function getParticipants(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants');
     promise.then(printParticipants);
@@ -209,12 +155,12 @@ function printParticipants(promise){
     }
     for(let i = 0; i < promise.data.length; i++){
         if(toWho === promise.data[i].name){
-            participantsList.innerHTML +=  `<li onclick="selectToWho(this)">
+            participantsList.innerHTML +=  `<li class="selected" onclick="selectToWho(this)">
                                                 <div>
                                                     <ion-icon name="person-circle"></ion-icon>
-                                                    <span class="contact-name">${promise.data[i].name}</span>
+                                                    <div class="contact-span"><span class="contact-name">${promise.data[i].name}</span><div>
                                                 </div>
-                                                <img class="selected" src="images/checkmark.png"/>
+                                                <img src="images/checkmark.png"/>
                                             </li>`;
         }
         else{
@@ -228,12 +174,10 @@ function printParticipants(promise){
         }
     }
 }
-
 function selectToWho(contact){
     toWho = contact.querySelector('.contact-name').innerHTML;
     getParticipants();
 }
-
 function selectVisibility(option){
     const options = document.querySelector(".visibility-options");
     const selectedOption = options.querySelector('.selected');
@@ -250,8 +194,6 @@ function selectVisibility(option){
         whoCanSee = 'private_message';
     }
 }
-
-//envio com enter
 document.addEventListener("keypress", function(e) {
     if(e.key === 'Enter') {
         sendMessage();
